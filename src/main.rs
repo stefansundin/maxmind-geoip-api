5d997a75f904460a7c309793932ac4dc73298e0d
Stefan Sundin <git@stefansundin.com>
2024-04-01 22:11:34 -0700

    Add `/metadata` endpoint.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,46 @@
 use actix_web::{get, middleware, web, App, HttpResponse, HttpServer};
 use log::{debug, error};
-use maxminddb::{geoip2, Reader};
+use maxminddb::{geoip2, Metadata, Reader};
+use serde::{Deserialize, Serialize};
 use serde_json::json;
-use std::{env, net::IpAddr, process};
+use std::{collections::BTreeMap, env, net::IpAddr, process};
 
 pub mod utils;
+
+#[derive(Serialize, Deserialize)]
+#[serde(remote = "Metadata")]
+struct MetadataDef {
+  binary_format_major_version: u16,
+  binary_format_minor_version: u16,
+  build_epoch: u64,
+  database_type: String,
+  description: BTreeMap<String, String>,
+  ip_version: u16,
+  languages: Vec<String>,
+  node_count: u32,
+  record_size: u16,
+}
+
+#[derive(serde::Serialize)]
+struct MetadataWrapper<'a>(#[serde(with = "MetadataDef")] &'a Metadata);
+
+#[get("/metadata")]
+async fn metadata() -> Result<HttpResponse, actix_web::error::Error> {
+  let reader = Reader::open_mmap(utils::database_path()).map_err(|err| {
+    error!("Error opening database: {}", err);
+    actix_web::error::ErrorInternalServerError(err.to_string())
+  })?;
+  debug!("{:?}", reader.metadata);
+
+  let body =
+    serde_json::to_string(&MetadataWrapper(&reader.metadata)).expect("error serializing metadata");
+
+  return Ok(
+    HttpResponse::Ok()
+      .append_header(("content-type", "application/json"))
+      .body(body),
+  );
+}
 
 #[get("/{ip}")]
 async fn lookup(addr: web::Path<IpAddr>) -> Result<HttpResponse, actix_web::error::Error> {
@@ -46,13 +82,16 @@ async fn main() -> std::io::Result<()> {
     .unwrap();
 
   HttpServer::new(|| {
-    App::new().service(lookup).wrap(middleware::Logger::new(
-      env::var("ACCESS_LOG_FORMAT")
-        .unwrap_or(String::from(
-          r#"%{r}a "%r" %s %b "%{Referer}i" "%{User-Agent}i" %T"#,
-        ))
-        .as_str(),
-    ))
+    App::new()
+      .service(metadata)
+      .service(lookup)
+      .wrap(middleware::Logger::new(
+        env::var("ACCESS_LOG_FORMAT")
+          .unwrap_or(String::from(
+            r#"%{r}a "%r" %s %b "%{Referer}i" "%{User-Agent}i" %T"#,
+          ))
+          .as_str(),
+      ))
   })
   .bind((host.as_str(), port))?
   .run()
